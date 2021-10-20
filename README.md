68228d5022aa3dbee1ae1341da5737e8cef988e8
CarlosDevlpr <78228463+CarlosDevlpr@users.noreply.github.com>
2021-10-20 16:27:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+**Português :brazil:**
+
+**Criei este projeto para aprimorar minhas habilidades em CSS e Javascript**
+
+_O projeto se resume em uma lista de a fazeres, inclui a função de adicionar tarefa e remove-lá!_
+
+Para ver o projeto, acesse este link:
+https://codepen.io/carlosdiamantino/pen/QWMLLgv
+
+**English 🇺🇸**
+
+**I created this project to improve my CSS and Javascript skills**
+
+_The project is summarized in a to-do list, includes the function to add task and remove it!_
+
+To see the project, visit this link:
+https://codepen.io/carlosdiamantino/pen/QWMLLgv
---
+++ js/script.js
@@ -0,0 +1,49 @@
+// Criar um novo item na lista quando clicar em Adicionar
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
+
+// Criar um botão de fechar e adicionar a todos os itens da lista
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Botão para esconder o 
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+
